b60f92c8639ec4efddd9a6f313c74ea5187ceb56
millo-cmd <3612emiliogonzalez@gmail.com>
2024-05-23 20:19:42 -0600

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibroDiarioComponent } from './components/libro-diario/libro-diario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibroDiarioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/libro-diario/libro-diario.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-libro-diario',
+  templateUrl: './libro-diario.component.html',
+  styleUrl: './libro-diario.component.css'
+})
+export class LibroDiarioComponent {
+ constructor() {
+  
+ }
+ 
+ funcion(){
+
+ }
+}
